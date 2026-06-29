08b8c865925240dfb2fb1f89a93ae4bf713cffe2
Wuİ <game.just45@gmail.com>
2026-06-29 22:15:06 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 -- WuiHub Loader
 -- Bu kodu executor'a yapıştır
-loadstring(game:HttpGet("https://raw.githubusercontent.com/D11KK4T/WuiHub/main/WuiHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D11KK4T/WuiHub/refs/heads/main/TOROSHUB-SKE.lua"))()
